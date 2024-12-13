3c8028514a49639bb88bace98a4cfde2d9ddfb9a
Andreas Wälchli <andreas.waelchli@me.com>
2024-12-13 00:24:39 +0000

    emulator: first component: 32bit data bus

--- emulator/src/main/kotlin/ch/awae/custom8bitemulator/hardware/DataBus.kt
+++ emulator/src/main/kotlin/ch/awae/custom8bitemulator/hardware/DataBus.kt
@@ -1,131 +1,68 @@
 package ch.awae.custom8bitemulator.hardware
 
+import ch.awae.custom8bitemulator.*
 
-sealed interface BusState<T : Any> {
-    fun merge(other: BusState<T>): BusState<T>
+/**
+ * Implements a data bus of up to 32bits.
+ *
+ * In case of contention, the offending bit is pulled low
+ *
+ * @param pullHigh if `true`, an unloaded bus (no component driving it actively) is pulled high.
+ *                 if `false`, an unloaded bus is pulled low.
+ */
+class DataBus(private val pullHigh: Boolean) : SimulationElement(ElementType.WIRE) {
+    private val idleState = if (pullHigh) 0xffffffffu else 0x00000000u
 
-    fun tryValue(): T
+    private val drivers = mutableMapOf<Driver, UInt>()
 
-    class Conflict<T : Any> : BusState<T> {
+    var state: UInt = idleState
+        private set
+    var contention: UInt = 0x00000000u
+        private set
 
-        override fun tryValue(): T {
-            throw UnsupportedOperationException("bus in conflict")
-        }
+    private var dirty = false
 
-        override fun merge(other: BusState<T>): BusState<T> {
-            return this
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return other is Floating<*>
-        }
-
-        override fun hashCode(): Int {
-            return -1
-        }
+    private fun drive(driver: Driver, value: UInt) {
+        drivers[driver] = value
+        dirty = true
     }
 
-    class Floating<T : Any> : BusState<T> {
-        override fun tryValue(): T {
-            throw UnsupportedOperationException("bus floating")
-        }
-
-        override fun merge(other: BusState<T>): BusState<T> {
-            return other
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return other is Floating<*>
-        }
-
-        override fun hashCode(): Int {
-            return 0
-        }
+    private fun release(driver: Driver) {
+        drivers.remove(driver)
+        dirty = true
     }
 
-    class BusValue<T : Any>(val value: T) : BusState<T> {
-        override fun tryValue(): T {
-            return value
-        }
-
-        override fun merge(other: BusState<T>): BusState<T> {
-            return if (other is BusValue) {
-                Conflict()
-            } else {
-                other.merge(this)
-            }
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return other is BusValue<*> && value == other.value
-        }
-
-        override fun hashCode(): Int {
-            return value.hashCode()
-        }
-    }
-
-    companion object {
-        fun <T : Any> of(value: T?): BusState<T> {
-            return if (value == null) {
-                Floating()
-            } else {
-                BusValue(value)
-            }
-        }
-
-        fun <T : Any> floating(): BusState<T> = Floating()
-        fun <T : Any> conflict(): BusState<T> = Conflict()
-    }
-}
-
-fun interface BusConsumer<T : Any> {
-    fun consume(data: BusState<T>)
-}
-
-interface DataBus<T : Any> {
-    fun connect(consumer: BusConsumer<T>)
-    val currentState: BusState<T>
-}
-
-class WritableBus<T : Any>(initial: T? = null) : DataBus<T> {
-
-    private val sourceStates: MutableMap<Any, T?> = mutableMapOf()
-    private val consumers: MutableSet<BusConsumer<T>> = mutableSetOf()
-    private var _currentState: BusState<T> = BusState.of(initial)
-
-    override val currentState: BusState<T>
-        get() = _currentState
-
-    fun push(source: Any, value: T?) {
-        if (sourceStates[source] == value) {
-            // no update, ignore update to prevent cycles
+    override fun tick(tickID: Long) {
+        if (!dirty) {
             return
         }
+        dirty = false
 
-        if (value == null) {
-            sourceStates.remove(source)
+        if (drivers.isEmpty()) {
+            state = idleState
+            contention = 0x00000000u
         } else {
-            sourceStates[source] = value
-        }
-
-        val previousState = _currentState
-
-        _currentState = sourceStates.values.fold(BusState.floating()) { acc, busValue ->
-            acc.merge(BusState.of(busValue))
-        }
-
-        if (previousState != _currentState) {
-            consumers.forEach { it.consume(_currentState) }
+            state = drivers.values.reduce(UInt::and)
+            contention = drivers.values.reduce(UInt::or) and state.inv()
         }
     }
 
-    override fun connect(consumer: BusConsumer<T>) {
-        consumers.add(consumer)
-        consumer.consume(_currentState)
+    interface Driver {
+        fun set(value: UInt)
+        fun release()
     }
 
+    fun connectDriver(): Driver {
+        return DriverImpl()
+    }
+
+    private inner class DriverImpl : Driver {
+        override fun set(value: UInt) {
+            this@DataBus.drive(this, value)
+        }
+
+        override fun release() {
+            this@DataBus.release(this)
+        }
+    }
 }
-
-typealias Signal = DataBus<Boolean>
-typealias WritableSignal = WritableBus<Boolean>
