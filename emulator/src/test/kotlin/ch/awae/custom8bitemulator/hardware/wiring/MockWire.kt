170663c58af9906f0fcea54fc27c4bcbb0f70f48
Andreas Wälchli <andreas.waelchli@me.com>
2024-12-16 22:42:47 +0000

    emulator: introduce wiring package

--- emulator/src/test/kotlin/ch/awae/custom8bitemulator/hardware/wiring/MockWire.kt
+++ emulator/src/test/kotlin/ch/awae/custom8bitemulator/hardware/wiring/MockWire.kt
@@ -4,3 +4,8 @@ class MockBus : DataBus {
     override var state: UInt = 0u
     override var contention: UInt = 0u
 }
+
+class MockSignal : DataSignal {
+    override var state: Boolean = false
+    override var contention: Boolean = false
+}
