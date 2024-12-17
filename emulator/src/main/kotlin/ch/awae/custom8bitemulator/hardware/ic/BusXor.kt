02e4f35c5ea33a2b567f7e2b2000663fd1ed4d1f
Andreas Wälchli <andreas.waelchli@me.com>
2024-12-17 20:23:39 +0000

    emulator: add bus Xor

---
+++ emulator/src/main/kotlin/ch/awae/custom8bitemulator/hardware/ic/BusXor.kt
@@ -0,0 +1,23 @@
+package ch.awae.custom8bitemulator.hardware.ic
+
+import ch.awae.custom8bitemulator.*
+import ch.awae.custom8bitemulator.hardware.wiring.*
+
+/**
+ * Octal Xor.
+ *
+ * Simulates every bit of an 8-bit bus xor-ed with the control signal.
+ */
+class BusXor(
+    private val input: DataBus,
+    private val control: DataSignal,
+    output: WritableBus,
+) : SimulationElement(ElementType.COMPONENT) {
+
+    private val outputDriver = output.connectDriver()
+
+    override fun tick(tickID: Long) {
+        outputDriver.set(input.state xor if (control.state) 0xffu else 0x00u)
+    }
+
+}
