e5720c370d0561d6daa8bfca3078fd4b0d2aa6f6
Andreas Wälchli <andreas.waelchli@me.com>
2024-12-23 13:51:29 +0000

    emulator: add roll unit

--- emulator/src/test/kotlin/ch/awae/custom8bitemulator/hardware/composites/RollUnitTest.kt
+++ emulator/src/test/kotlin/ch/awae/custom8bitemulator/hardware/composites/RollUnitTest.kt
@@ -9,8 +9,8 @@ import kotlin.test.*
 class RollUnitTest {
 
     @ParameterizedTest
-    @MethodSource("combinations")
-    fun test(command: Int, input: Int, carryIn: Boolean, expectedOutput: Int?, expectedCarryOut: Boolean) {
+    @CsvFileSource(resources = ["/roll_unit_test.csv"], delimiter = ';', numLinesToSkip = 1)
+    fun test(command: Int, input: Int, carryIn: Boolean, expectedOutput: Int, expectedCarryOut: Boolean) {
         val inputA = StandardWritableBus(false)
         val control = StandardWritableBus(false)
         val cIn = StandardWritableSignal(false)
@@ -25,35 +25,8 @@ class RollUnitTest {
 
         sim.tick(100)
 
-        assertEquals((expectedOutput ?: 0).toUInt(), output.state)
+        assertEquals((expectedOutput.takeUnless { it < 0 } ?: 0).toUInt(), output.state)
         assertEquals(expectedCarryOut, carryOut.state)
-    }
-
-    companion object {
-        @JvmStatic
-        fun combinations(): List<Arguments?> = (0..3).flatMap { cmd ->
-            (0..255).flatMap { input ->
-                listOf(false, true).map { cin ->
-                    if (cmd == 3) {
-                        Arguments.of(3, input, cin, null, input % 2 != 0)
-                    } else if (cmd == 1) {
-                        if (cin) {
-                            Arguments.of(1, input, true, ((input shl 1) + 1) and 0xff, (input and 0x80) != 0)
-                        } else {
-                            Arguments.of(1, input, false, (input shl 1) and 0xff, (input and 0x80) != 0)
-                        }
-                    } else if (cmd == 2) {
-                        if (cin) {
-                            Arguments.of(2, input, true, ((input shr 1) + 128) and 0xff, input % 2 != 0)
-                        } else {
-                            Arguments.of(2, input, false, (input shr 1) and 0xff, input % 2 != 0)
-                        }
-                    } else {
-                        Arguments.of(0, input, cin, ((input * 16) + (input / 16)) and 0xff, (input and 0x80) != 0)
-                    }
-                }
-            }
-        }
     }
 
 }
